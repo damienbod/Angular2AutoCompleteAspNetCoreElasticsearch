1083ec34ba31d9fe8e2c09a707280f826eba1e65
damienbod <damien_bod@hotmail.com>
2016-10-11 20:25:40 +0200

    adding the search component

--- src/Angular2AutoCompleteAspNetCoreElasticsearch/angular2App/app/autocomplete/search.component.ts
+++ src/Angular2AutoCompleteAspNetCoreElasticsearch/angular2App/app/autocomplete/search.component.ts
@@ -1,30 +1,50 @@
-import { Component, EventEmitter, Input, Output, OnInit, ElementRef} from '@angular/core';
+import { Component, Inject, EventEmitter, Input, Output, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+
+
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import { Router } from  '@angular/router';
 
-declare var Plotly: any;
+import { SearchDataService } from '../autocomplete/searchDataService';
+
+
+import { CompleterService, CompleterData } from 'ng2-completer';
 
 @Component({
     selector: 'autocompletesearch',
   template: `
-<div style="margin-bottom:100px;">
-    todo
-</div>
+<ng2-completer [(ngModel)]="searchStr" [dataService]="dataService" [minSearchLength]="0"></ng2-completer>
 
 `
 })
 
 
-export class AutocompleteSearchComponent implements OnInit {
+export class AutocompleteSearchComponent implements OnInit    {
+
+    constructor(private completerService: CompleterService) {
+        this.dataService = completerService.local(this.searchData, 'color', 'color');
+    }
 
     @Input() data: any;
 
+    private searchStr: string;
+    private dataService: CompleterData;
+    private searchData = [
+        { color: 'red', value: '#f00' },
+        { color: 'green', value: '#0f0' },
+        { color: 'blue', value: '#00f' },
+        { color: 'cyan', value: '#0ff' },
+        { color: 'magenta', value: '#f0f' },
+        { color: 'yellow', value: '#ff0' },
+        { color: 'black', value: '#000' }
+    ];
 
     ngOnInit() {
         console.log("ngOnInit AutocompleteSearch");
-        console.log(this.data);
+        console.log(this.searchData);
 
-       // Plotly.newPlot('myPlotlyDiv', this.data, this.layout, this.options);
     }
+
+
+
 }
