a1a4c705b4c12455ed406e69ef4aa053f917a63f
damienbod <damien_bod@hotmail.com>
2016-10-12 17:19:10 +0200

    Web API methods to create index, check index status and add some test data

--- src/Angular2AutoCompleteAspNetCoreElasticsearch/Controllers/PersonCityController.cs
+++ src/Angular2AutoCompleteAspNetCoreElasticsearch/Controllers/PersonCityController.cs
@@ -33,7 +33,21 @@ namespace Angular2AutoCompleteAspNetCoreElasticsearch.Controllers
             //return Ok(_personCitySearchProvider.QueryString("*").Select(t => new CompleterItem { OriginalObject = t, Title = t.Name, Description= t.Info }));
         }
 
-        [HttpGet("ElasticsearchStatus")]
+        [HttpGet("createindex")]
+        public IActionResult CreateIndex()
+        {
+            _personCitySearchProvider.CreateIndex();
+            return Created("http://localhost:5000/api/PersonCity/createindex/", "index created");
+        }
+
+        [HttpGet("createtestdata")]
+        public IActionResult CreateTestData()
+        {
+            _personCitySearchProvider.CreateTestData();
+            return Created("http://localhost:5000/api/PersonCity/createtestdata/", "test data created");
+        }
+
+        [HttpGet("indexexists")]
         public IActionResult GetElasticsearchStatus()
         {
             return Ok(_personCitySearchProvider.GetStatus());
